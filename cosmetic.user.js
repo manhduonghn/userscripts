1d35d3380beb21ec669f93a21460ef5fddb1156a
manhduonghn <manhduonghn@users.noreply.github.com>
2024-11-14 01:05:00 +0000

    Cập nhật userscript: 2024-11-14

--- cosmetic.user.js
+++ cosmetic.user.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name         Cosmetic Ad Block for Bromite
 // @namespace    manhduonghn
-// @version      2024.11.13
+// @version      2024.11.14
 // @description  Blocks annoying elements in pages, sourced from many different filter lists
 // @author       manhduonghn
 // @match        *://*/*
@@ -15,7 +15,7 @@
 /// @stats blockers for 9 domains, injected CSS rules for 0 domains
 {
     let log = function (...data) {
-        console.log("[Cosmetic filters by manhduonghn (v2024.11.13 full)]:", ...data);
+        console.log("[Cosmetic filters by manhduonghn (v2024.11.14 full)]:", ...data);
     }
 
 
